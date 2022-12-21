6562b51f5c714f78f8216548f281f7996d600781
Clara <clarapachecodev@gmail.com>
2022-12-21 17:19:34 -0300

    Refacture element captures into variables assignments

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const rollDice = document.querySelector('.btn--roll')
+let diceImage = document.querySelector('.dice')
+
+
+// ============ Player 1 ================================
+const score1 = document.querySelector('#score--0')
+let currentPoints1 = document.querySelector('#current--0')
+
+
+// ============ Player 2 ================================
+const score2 = document.querySelector('#score--1')
+let currentPoints2 = document.querySelector('#current--1')
+
+
 
 rollDice.addEventListener('click',function(){
   let diceNumber = Math.abs(Math.round(Math.random() * 10) - 4)
@@ -6,32 +19,32 @@ rollDice.addEventListener('click',function(){
   if(diceNumber !== 0){
     switch(diceNumber){
       case 1:
-        document.querySelector('.dice').src = 'img/dice-1.png'
-        document.querySelector('#current--0').textContent = 0
+        diceImage.src = 'img/dice-1.png'
+        currentPoints1.textContent = 0
         break
       case 2:
-        document.querySelector('.dice').src = 'img/dice-2.png'
-        document.querySelector('#current--0').textContent = Number(document.querySelector('#current--0').textContent) + 2
+        diceImage.src = 'img/dice-2.png'
+        currentPoints1.textContent = Number(currentPoints1.textContent) + 2
         
         break
       case 3:
-        document.querySelector('.dice').src = 'img/dice-3.png'
-        document.querySelector('#current--0').textContent = Number(document.querySelector('#current--0').textContent) + 3
+        diceImage.src = 'img/dice-3.png'
+        currentPoints1.textContent = Number(currentPoints1.textContent) + 3
         
         break
       case 4:
-        document.querySelector('.dice').src = 'img/dice-4.png'
-        document.querySelector('#current--0').textContent = Number(document.querySelector('#current--0').textContent) + 4
+        diceImage.src = 'img/dice-4.png'
+        currentPoints1.textContent = Number(currentPoints1.textContent) + 4
       
         break
       case 5:
-        document.querySelector('.dice').src = 'img/dice-5.png'
-        document.querySelector('#current--0').textContent = Number(document.querySelector('#current--0').textContent) + 5
+        diceImage.src = 'img/dice-5.png'
+        currentPoints1.textContent = Number(currentPoints1.textContent) + 5
      
         break
       case 6:
-        document.querySelector('.dice').src = 'img/dice-6.png'
-        document.querySelector('#current--0').textContent = Number(document.querySelector('#current--0').textContent) + 6
+        diceImage.src = 'img/dice-6.png'
+        currentPoints1.textContent = Number(currentPoints1.textContent) + 6
        
         break
     }
@@ -42,8 +55,8 @@ rollDice.addEventListener('click',function(){
 const holdBtn = document.querySelector('.btn--hold')
 
 holdBtn.addEventListener('click',function(){
-  document.querySelector('#score--0').textContent = document.querySelector('#current--0').textContent
-  if (document.querySelector('#score--0').textContent >= 100){
+  score1.textContent = currentPoints1.textContent
+  if (score1.textContent >= 100){
     console.log('You win!')
   }
 })
@@ -52,4 +65,5 @@ const newGameBtn = document.querySelector('.btn--new')
 
 newGameBtn.addEventListener('click',function(){
   window.location.reload(true)
+  diceImage.classList.add('dice-hidden')
 })
